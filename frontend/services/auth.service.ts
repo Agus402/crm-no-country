b192d89163d49575a7c08725cea0301402f096d7
lorenzosegada <lorenzosegada@gmail.com>
2025-12-02 20:20:51 -0300

    hotfix:backend Url local

--- frontend/services/auth.service.ts
+++ frontend/services/auth.service.ts
@@ -1,4 +1,4 @@
-const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://192.168.1.84:8080/api";
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080/api";
 
 export const authService = {
     async login(email: string, password: string) {
