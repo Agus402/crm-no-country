fa22331978294ccbd4f284f0bb778929599ff36a
Brian Gamarra <brian7u7ramdon@gmail.com>
2025-12-11 16:59:03 -0300

    test automated workflows

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -6,6 +6,16 @@ export default defineConfig({
     specPattern: "cypress/e2e/**/*.cy.ts",
     supportFile: "cypress/support/e2e.ts",
     video: false,
+    screenshotOnRunFailure: true,
+    defaultCommandTimeout: 10000,
+    requestTimeout: 10000,
+    responseTimeout: 10000,
+    viewportWidth: 1280,
+    viewportHeight: 720,
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+      return config;
+    },
   },
 });
 
