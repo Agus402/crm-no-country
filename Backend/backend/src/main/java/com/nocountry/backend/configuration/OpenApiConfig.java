180646c0c80915da19be21ea599a87a5337a61e3
Jorge Subeldia <54650566+jhorgesub@users.noreply.github.com>
2025-12-11 19:59:30 -0300

    Implementado Swagger

---
+++ Backend/backend/src/main/java/com/nocountry/backend/configuration/OpenApiConfig.java
@@ -0,0 +1,19 @@
+package com.nocountry.backend.configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("CRM Startup API")
+                        .version("1.0.0")
+                        .description("API REST para la gestión de Contactos, Tareas e Integración con WhatsApp y Correo electrónico."));
+    }
+}
